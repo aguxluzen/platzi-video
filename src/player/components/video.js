74c7b58cdff87449b2395974029c478627f92a4c
Agustin Luzenti <aguss.luzenti@gmail.com>
2018-06-09 16:58:21 -0300

    Clase 29 terminada

--- src/player/components/video.js
+++ src/player/components/video.js
@@ -1,17 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './video.css';
 
 class Video extends Component {
-  render() {
-    return (
-      <div className="Video">
-        <video
-            autoPlay={this.props.autoplay}
-            src={this.props.src}
-        />
-      </div>
-    )
-  }
+
+    tooglePlay() {
+        if (this.props.pause) {
+            this.video.play();
+        } else {
+            this.video.pause();
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.pause !== this.props.pause) {
+            this.tooglePlay();
+        }
+    }
+
+    setRef = (element) => {
+        this.video = element;
+    };
+
+    render() {
+        return (
+            <div className="Video">
+                <video
+                    autoPlay={this.props.autoplay}
+                    src={this.props.src}
+                    ref={this.setRef}
+                />
+            </div>
+        )
+    }
 }
 
 export default Video;
--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,22 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
+import PlayPause from '../components/play-pause';
 
 class VideoPlayer extends Component {
-  render() {
-    return (
-      <VideoPlayerLayout>
-          <Title
-            title="Esto es un video"
-          />
-        <Video
-            autoplay={true}
-            src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
-        />
-      </VideoPlayerLayout>
-    )
-  }
+    state = {
+        pause: true
+    };
+
+    tooglePlay = (event) => {
+        this.setState({
+            pause: !this.state.pause
+        })
+    };
+
+    componentDidMount() {
+        this.setState({
+            pause: (!this.props.autoplay)
+        });
+    }
+
+    render() {
+        return (
+            <VideoPlayerLayout>
+                <Title
+                    title="Esto es un video"
+                />
+                <PlayPause
+                    pause={this.state.pause}
+                    handleClick={this.tooglePlay}
+                />
+                <Video
+                    autoplay={this.props.autoplay}
+                    pause={this.state.pause}
+                    src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+                />
+            </VideoPlayerLayout>
+        )
+    }
 }
 
 export default VideoPlayer;
